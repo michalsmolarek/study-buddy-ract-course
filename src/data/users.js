1bbbe24fd7a27fcae7bfa8cd3d13ef1a8b022459
Michał Smolarek <msmolarek96@gmail.com>
2022-01-27 20:27:36 +0100

    mr

---
+++ src/data/users.js
@@ -0,0 +1,32 @@
+export const users = [
+  {
+    name: 'Jan Nowak',
+    average: '39%',
+    attendence: '2.3',
+  },
+  {
+    name: 'Adam Kowal',
+    average: '39%',
+    attendence: '2.3',
+  },
+  {
+    name: 'Tomasz Hajto',
+    average: '39%',
+    attendence: '2.3',
+  },
+  {
+    name: 'Jessica Holland',
+    average: '39%',
+    attendence: '2.3',
+  },
+  {
+    name: 'Audrey Kowalsky',
+    average: '39%',
+    attendence: '2.3',
+  },
+  {
+    name: 'John Doe',
+    average: '39%',
+    attendence: '2.3',
+  },
+];
---
+++ src/views/Root.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import UsersList from 'components/UsersList/UserList';
+
+const Root = () => (
+  <>
+    <UsersList />
+  </>
+);
+
+export default Root;
