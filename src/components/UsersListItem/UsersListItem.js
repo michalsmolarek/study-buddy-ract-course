4768f26cf128402193d6917a544c80030eeef63c
Michał Smolarek <msmolarek96@gmail.com>
2022-01-27 20:41:17 +0100

    added enter

---
+++ src/components/UsersListItem/UsersListItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const UsersListItem = ({ userData: { average, name, attendence } }) => (
+  <>
+    <li>
+      <div>{average}</div>
+      <div>
+        <p>{name}</p>
+        <p>{attendence}</p>
+      </div>
+      <button>x</button>
+    </li>
+  </>
+);
+
+UsersListItem.propTypes = {
+  userData: propTypes.shape({
+    average: propTypes.string,
+    name: propTypes.string.isRequired,
+    attendence: propTypes.string,
+  }),
+};
+
+
+export default UsersListItem;
