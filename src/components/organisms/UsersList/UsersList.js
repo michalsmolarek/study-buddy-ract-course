e101d5c04f426b83a0f7129f41582bab294831f9
Michał Smolarek <msmolarek96@gmail.com>
2022-02-06 18:50:58 +0100

    create component class

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -3,14 +3,27 @@ import { users } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { StyledList, Wrapper } from './UsersList.styles';
 
-const UsersList = () => (
-  <Wrapper>
-    <StyledList>
-      {users.map((userData, i) => (
-        <UsersListItem index={i} key={userData.name} userData={userData} />
-      ))}
-    </StyledList>
-  </Wrapper>
-);
+class UsersList extends React.Component {
+  state = {
+    users,
+  };
+
+  deleteUser = (name) => {
+    const filtereduser = this.state.users.filter(user => user.name !== name);
+    this.setState({users: filtereduser});
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <StyledList>
+          {this.state.users.map((userData, i) => (
+            <UsersListItem deleteUser={this.deleteUser} key={userData.name} userData={userData} />
+          ))}
+        </StyledList>
+      </Wrapper>
+    );
+  }
+}
 
 export default UsersList;
